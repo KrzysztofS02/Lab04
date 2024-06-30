3abbc4584c43c52b4c58c76ae94bcbb6fccf1c06
Krzysztof Stępień <kris2002@o2.pl>
2024-06-30 23:19:00 +0200

    Dodanie funkcji tworzenia plików logx.txt

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,11 @@
 if [ "$1" == "--date" ]; then
     date
 fi
+
+if [ "$1" == "--logs" ]; then
+    count=${2:-100}
+    for ((i=1; i<=count; i++)); do
+        echo "log${i}.txt created by skrypt.sh on $(date)" > "log${i}.txt"
+    done
+fi
+
