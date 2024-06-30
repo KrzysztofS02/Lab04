c92e069d2e32ac899d336c28d04c765f9abccb88
Krzysztof Stępień <kris2002@o2.pl>
2024-06-30 23:21:22 +0200

    Dodanie obsługi argumentu liczby plików

--- skrypt.sh
+++ skrypt.sh
@@ -11,3 +11,10 @@ if [ "$1" == "--logs" ]; then
     done
 fi
 
+if [ "$1" == "--logs" ]; then
+    count=${2:-100}
+    for ((i=1; i<=count; i++)); do
+        echo "log${i}.txt created by skrypt.sh on $(date)" > "log${i}.txt"
+    done
+fi
+
