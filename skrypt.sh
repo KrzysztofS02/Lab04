9dab4b7be12523af298b537496e0c5dfba39c563
Krzysztof Stępień <kris2002@o2.pl>
2024-06-30 23:25:46 +0200

    Dodanie obsługi flag z pojedynczym myślnikiem

--- skrypt.sh
+++ skrypt.sh
@@ -23,3 +23,24 @@ if [ "$1" == "--help" ]; then
     echo "--logs [number]: Tworzy określoną liczbę plików logx.txt"
     echo "--help: Wyświetla dostępne opcje"
 fi
+
+case "$1" in
+    --date|-d)
+        date
+        ;;
+    --logs|-l)
+        count=${2:-100}
+        for ((i=1; i<=count; i++)); do
+            echo "log${i}.txt created by skrypt.sh on $(date)" > "log${i}.txt"
+        done
+        ;;
+    --help|-h)
+        echo "--date -d: Wyświetla dzisiejszą datę"
+        echo "--logs -l [number]: Tworzy określoną liczbę plików logx.txt"
+        echo "--help -h: Wyświetla dostępne opcje"
+        ;;
+    *)
+        echo "Nieprawidłowa opcja. Użyj --help, aby zobaczyć dostępne opcje."
+        ;;
+esac
+
